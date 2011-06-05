ae9549bff34afb0c273df7db01e054e096ae240e
ccfreak2k <ccfreak2k@gmail.com>
2011-06-05 06:48:41 -0700

    added initial files

---
+++ besmcr.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""
+SYNOPSIS
+
+	besmcr.py
+
+DESCRIPTION
+
+	Python program to assist in making a BESM d20 character.
+
+AUTHOR
+
+	ccfreak2k
+
+LICENSE
+
+	This program is licensed under the terms of the BSD license.
+
+VERSION
+
+	$Id$
+"""
+
+def main ():
