53dc963f2d38d7930fbf1adb2b756b5ddae9b970
Alexandre Gauthier <alex@lab.underwares.org>
2011-08-05 10:36:27 -0400

    Add FIXME note for unsafe YAML loading

--- besmcr/character.py
+++ besmcr/character.py
@@ -5,88 +5,94 @@ from math import ceil
 
 class Character(object):
     """Represents a BESM character configuration."""
-    
+
     def __init__(self):
         # Character information
         self.name = ""
         self.age = ""
         self.genre = "Cyberpunk"
         self.notes = ""
-                
+
         # Appearance attributes
         self.race = ""
         self.height = ""
         self.weight = ""
-        
+
         # Points
         self.character_points = 35
-        
+
         # Stats
         self.body = 1
         self.mind = 1
         self.soul = 1
-        
+
         # Modifiers
         self.attributes = []
         self.skills = []
-    
+
     # --------------------------------------------------------------------------
-    
+
     def get_remaining_character_points(self):
         """Returns the remaining character points, as the name suggests"""
         return self.character_points - (self.body + self.mind + self.soul)
-        
+
     # --------------------------------------------------------------------------
-    
+
     def get_health(self):
         """Returns HP based on stats.
-        
+
         HP = (Body + Soul) * 5
         """
         return (self.body + self.soul) * 5
-        
+
     def get_energy(self):
         """Returns energy based on stats.
-        
+
         Energy = (Mind + Soul) * 5
         """
         return (self.mind + self.soul) * 5
-        
+
     def get_shock(self):
         """Returns shock value based on stats.
-        
+
         Shock = (HP / 5) (rounded up)
         """
         return int(ceil(self.get_health() / 5.0))
-        
+
     def get_combat(self):
         """Returns combat value based on stats.
-        
+
         Combat = (Body + Mind + Soul) / 3 (rounded up)
         """
         return int(ceil((self.body + self.mind + self.soul) / 3.0))
-        
+
     def get_defense(self):
         """Returns defense value based on stats.
-        
+
         Defense = (Combat - 2)
         """
         return self.get_combat() - 2
-        
+
     # --------------------------------------------------------------------------
-    
+
     def save(self, filename):
-        """Saves the character as a YAML file"""     
+        """Saves the character as a YAML file"""
         stream = file(filename, 'w')
         yaml.dump(self, stream)
 
 # ==============================================================================
 
 def load(filename):
-    """Loads a character from a YAML file and returns it"""  
+    """Loads a character from a YAML file and returns it"""
+
+    # FIXME: This is horrible and unsafe. No need to serialize the
+    #        class, arguably all you'd need is a structured data tree
+    #        encountaining all the values. Those can be read back to
+    #        python types and then associated. I will fix this. -D.
+
     stream = file(filename, 'r')
     c = yaml.load(stream)
-    
+
     if isinstance(c, Character):
         return c
-        
+
